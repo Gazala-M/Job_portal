899112815f1e1c318cd607c43af25ea8fb317855
Gazala-M <128143047+Gazala-M@users.noreply.github.com>
2023-08-30 14:32:51 +0530

    Update resp.js

--- resp.js
+++ resp.js
@@ -76,8 +76,8 @@ $('.slider').slick({
     speed: 300,
     slidesToShow: 3,
     slidesToScroll: 1,
-  /*  autoplay: true,
-    autoplaySpeed: 2000,  */
+    autoplay: true,
+    autoplaySpeed: 2000,  
     responsive: [
       {
         breakpoint: 1024,
